7796b9a872d92fd200a5f5604ca02418fe603c37
jonathan-edwards-uk <jone9999@yahoo.co.uk>
2015-05-12 15:04:20 +0100

    Added first pass implementation of migration sql from old timeTracker db to new timelog db

---
+++ Timelog/MigrateTriggers.sql
@@ -0,0 +1,31 @@
+﻿
+/*********************
+ * After insert tridder on old TimeEntry table 
+ */
+
+ IF OBJECT_ID('TRG_InsertSyncTimEntries') IS NOT NULL
+ DROP TRIGGER TRG_InsertSyncTimEntries
+ GO
+
+ CREATE TRIGGER TRG_InsertSyncTimEntries
+ ON [TimeTracker].[dbo].[aspnet_starterkits_TimeEntry]
+ AFTER INSERT AS
+ BEGIN
+INSERT INTO [TimeLog].[dbo].[TimeEntry]
+           ([TimeEntryCreated]
+           ,[TimeEntryDuration]
+           ,[TimeEntryDescription]
+           ,[TimeEntryDate]
+           ,[BookingCodeId]
+           ,[TimeEntryCreatorId]
+           ,[TimeEntryUserId])
+     Select 
+      ,[TimeEntryCreated]
+      ,[TimeEntryDuration]
+      ,[TimeEntryDescription]     
+      ,[TimeEntryDate]
+	  ,[CategoryId]
+      ,[TimeEntryCreatorId]
+      ,[TimeEntryUserId] from INSERTED
+ END
+ GO
