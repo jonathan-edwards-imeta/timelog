181f69be317503f35518a77a7268e5728e71175e
jonathan-edwards-uk <jone9999@yahoo.co.uk>
2015-06-12 17:48:33 +0100

    SecurityImplementation Complete up to end of tutorial 2

---
+++ Timelog/Security/Migrations/Configuration.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Security.Infrastructure;
+
+namespace Security.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Security.Infrastructure.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Security.Infrastructure.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "SuperPowerUser",
+                Email = "test@imeta.com",
+                EmailConfirmed = true,
+                FirstName = "John",
+                LastName = "Smith",
+                Level = 1,
+                JoinDate = DateTime.Now.AddYears(-4)
+            };
+
+            manager.Create(user, "MySuperP@ssword!");
+        }
+    }
+}
---
+++ Timelog/Security/Validators/iMetaEmailUserValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Security.Infrastructure;
+
+
+namespace Security.Validators
+{
+    public class ImetaEmailUserValidator : UserValidator<ApplicationUser>
+    {
+        readonly List<string> _allowedEmailDomains = new List<string> { "imeta.co.uk" };
+
+        public ImetaEmailUserValidator(ApplicationUserManager appUserManager)
+            : base(appUserManager)
+        {
+        }
+
+        public override async Task<IdentityResult> ValidateAsync(ApplicationUser user)
+        {
+            IdentityResult result = await base.ValidateAsync(user);
+
+            var emailDomain = user.Email.Split('@')[1];
+
+            if (!_allowedEmailDomains.Contains(emailDomain.ToLower()))
+            {
+                var errors = result.Errors.ToList();
+
+                errors.Add($"Email domain '{emailDomain}' is not allowed");
+
+                result = new IdentityResult(errors);
+            }
+
+            return result;
+        }
+    }
+}
